ddba54ad343b909d4e5a554821da13b011f70210
Luca Soler <luca.soler@edu.ece.fr>
2023-12-05 10:25:42 +0100

    wip

--- game_menu.py
+++ game_menu.py
@@ -1,6 +1,7 @@
 import pygame
-from code_numpy import jeu_principal
-
+from game_engine import GameOfLife
+from game_renderer import GameOfLifeGUI
+from game_grid_size import GridSizeMenu
 
 class GameMenu:
     def __init__(
@@ -125,10 +126,10 @@ class GameMenu:
                     self.choose_grid_size()
                     if self.playing:
                         self.user_input = int(self.user_input)
-                        jeu_principal(self.user_input)
+                        game_launcher(self.user_input)
                         mainloop = False
                 elif self.selected_action == "Charger":
-                    jeu_principal(load=True)
+                    game_launcher(load=True)
                     self.settings_selected = True
                     mainloop = False
                 elif self.selected_action == "Quitter":
@@ -138,99 +139,10 @@ class GameMenu:
             pygame.display.flip()
 
 
-class GridSizeMenu:
-    def __init__(
-        self,
-        screen,
-        game_menu,
-        font=None,
-        font_size=50,
-        background_image_path="image/fond_noel.jpg",
-    ):
-        self.screen = screen
-        self.scr_width = self.screen.get_rect().width
-        self.scr_height = self.screen.get_rect().height
+def game_launcher(taille=50, load=False):
+    game = GameOfLife(width=taille, height=taille)
+    if load:
+        game.load_from_file("game_state.txt")
+    gui = GameOfLifeGUI(game)
 
-        self.bg_color = (0, 0, 0)
-        self.paddingx = 8
-        self.paddingy = 20
-
-        self.index_selected = 0
-        self.font = pygame.font.Font(font, font_size)
-        self.current_item = ""
-        self.game_menu = game_menu
-
-        # self.user_input = ""
-        self.background_image = pygame.image.load(background_image_path)
-
-    def run(self):
-        choix_loop = True
-        while choix_loop:
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    choix_loop = False
-
-                if event.type == pygame.KEYDOWN:
-                    if event.key == pygame.K_ESCAPE:
-                        self.game_menu.selected_action = None
-                        self.game_menu.start_selected = False
-                        choix_loop = False
-
-                    if event.key == pygame.K_RETURN:
-                        try:
-                            selected_size = int(self.game_menu.user_input)
-                            print(
-                                f"Taille de la grille sélectionnée : {selected_size}x{selected_size}"
-                            )
-                            if selected_size >= 50:
-                                # FAIRE LA CREATION DE GRILLE ICI
-                                self.game_menu.selected_action = None
-                                self.game_menu.start_selected = False
-                                self.game_menu.playing = True
-                                choix_loop = False
-
-                        except ValueError:
-                            print("Veuillez entrer un nombre valide.")
-
-                    if event.key == pygame.K_BACKSPACE:
-                        self.game_menu.user_input = self.game_menu.user_input[:-1]
-
-                    if event.key in (
-                        pygame.K_0,
-                        pygame.K_1,
-                        pygame.K_2,
-                        pygame.K_3,
-                        pygame.K_4,
-                        pygame.K_5,
-                        pygame.K_6,
-                        pygame.K_7,
-                        pygame.K_8,
-                        pygame.K_9,
-                    ):
-                        self.game_menu.user_input += event.unicode
-
-            scaled_background = pygame.transform.scale(
-                self.background_image, (self.scr_width, self.scr_height)
-            )
-            self.screen.blit(scaled_background, (0, 0))
-
-            pygame.draw.rect(
-                self.screen,
-                (255, 255, 255),
-                (self.scr_width / 2 - 100, self.scr_height / 2 - 10, 200, 40),
-                2,
-            )
-
-            label1 = self.font.render(f"Taille de la grille > 50 :", 1, (255, 255, 255))
-            width = label1.get_rect().width
-            height = label1.get_rect().height + 20
-            posx = (self.scr_width / 2) - (width / 2)
-            posy = (self.scr_height / 2) - height
-            self.screen.blit(label1, (posx, posy))
-
-            label = self.font.render(f"{self.game_menu.user_input} ", 1, (16, 25, 80))
-            posx = self.scr_width / 2 - 10
-            posy = self.scr_height / 2 - 1
-            self.screen.blit(label, (posx, posy))
-
-            pygame.display.flip()
+    gui.run()
--- main.py
+++ main.py
@@ -1,50 +1,17 @@
-import os
-
-import matplotlib
-
-from GrilleElement import Grille
 import pygame
-from affichage import GameMenu
+from game_menu import GameMenu
 
 if __name__ == "__main__":
-    # créer la grille
-    # blindage pour être sur que la taille est un nb entier
-    ###
-    #
-    #     while True:
-    #         # demander la taille de la grille à l'utilisateur
-    #         lignes = input("saisir la taille N de la grille (NxN): ")
-    #         if lignes.isnumeric() or (
-    #             lignes[1:].isnumeric() and lignes[0] == "-"
-    #         ):  # verifie si le nb est un entier positif
-    #             # ou entier negatif : 1er terme - et 2eme terme un entier
-    #             break
-    #         else:
-    #             print("Veuillez saisir un entier")
-    #     lignes = int(lignes)  # pour caster le nb
-    #     colonnes = lignes
-    # grille = Grille(lignes, colonnes)
-    #
-    # ##
-
     # matplotlib.use("TkAgg")
-    # Initialisation de Pygame
+
     pygame.init()
-    # Taille de la fenêtre
     info = pygame.display.Info()
-    screen = pygame.display.set_mode((info.current_w, info.current_h))
+    screen = pygame.display.set_mode((info.current_w, info.current_h - 80))
     CLOCK_EVENT = pygame.USEREVENT + 1
     pygame.time.set_timer(
         CLOCK_EVENT, 300
-    )  # Générer l'événement toutes les 1000 millisecondes (1 seconde)
-
-    # Éléments du menu
+    )
     menu_items = ["Jouer", "Charger", "Quitter"]
-
-    # Création du menu
     menu = GameMenu(screen, menu_items)
-
-    # Exécution du menu
     menu.run()
 
-    # Boucle_principale(grille)
